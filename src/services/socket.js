315df49a195cd1546ca83d8914dff68896508b74
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-09-19 23:38:45 +0100

    change socket.io settings

--- src/services/socket.js
+++ src/services/socket.js
@@ -6,4 +6,9 @@ const socketUrl =
     ? 'http://localhost:3000/'
     : 'https://fakespeare-socket.herokuapp.com/';
 
-export const socket = io(socketUrl);
+export const socket = io(socketUrl, {
+  reconnection: true,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax: 5000,
+  reconnectionAttempts: 99999,
+});
